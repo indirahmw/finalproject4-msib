40084517ceb424522dff54411e8e5e675ab1137a
elxabrin <www.elsadwis@gmail.com>
2023-06-16 22:17:41 +0700

    gagal hide API

--- fp4/index.js
+++ fp4/index.js
@@ -1,6 +1,7 @@
 const searchButton = document.querySelector('#button-addon2');
 const inputKeyword = document.querySelector('.input-keyword');
 
+
 searchButton.addEventListener('click', function() {
 
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputKeyword.value+"&appid=1fe5f03e8b679377cbc41601289edfdd&units=metric")
